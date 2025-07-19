8acadb40c70aaed6091e5293849cedc92986a4b9
Kennet Tardivo <kennet.tardivo@gmail.com>
2025-07-19 17:24:47 +0100

    fix: Update base path to match repository name for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-js-cheatsheet/'
+  base: '/design-react-js-cheatsheet/'
 }) 
