2ce57b8bbfcb34ec79ef70cc5f836bf8d332189c
Kennet Tardivo <kennet.tardivo@gmail.com>
2025-07-20 18:39:49 +0100

    Fix GitHub Pages routing: Switch to HashRouter and add 404.html redirect

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Using HashRouter, so we don't need the base path for routing
+  // The base path is still needed for asset loading on GitHub Pages
   base: '/design-react-js-cheatsheet/'
 }) 
